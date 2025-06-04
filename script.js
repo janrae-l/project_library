0db65c91ccf780bb81d9aeebee229482ed2a5a2c
Janine Rae Labadia <105533574+janrae-l@users.noreply.github.com>
2025-06-04 17:01:04 +0800

    Add function for the unique ID for each book and test it out

--- script.js
+++ script.js
@@ -10,10 +10,18 @@ function Book(title, author, pages, read) {
   this.read = read;
 
   const uniqueId = crypto.randomUUID();
+  this.uniqueBookID = function () {
+    return uniqueId;
+  };
 }
 
 Book.prototype.addBookToLibrary = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
 
-const booKOne = new Book();
+const booKOne = new Book("Mockingjay", "Suzanne Collins", 390, "not read");
+
+console.log(booKOne.uniqueBookID());
+
+const bookTwo = new Book("Sample", "Sample", 200, "Sample");
+console.log(bookTwo.uniqueBookID());
