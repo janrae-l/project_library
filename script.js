1a5374f7f0b42a91a276d893756bbdf1e9468c7a
Janine Rae Labadia <105533574+janrae-l@users.noreply.github.com>
2025-06-04 16:55:59 +0800

    Add throw Error to the constructor, add unique ID generator and instantiate a book from the constructor

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+
+  const uniqueId = crypto.randomUUID();
 }
 
 Book.prototype.addBookToLibrary = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 };
+
+const booKOne = new Book();
